9e9c83639bf4f5a82acc60a2bf96216a7fd14b4b
Masaya Yamamoto <masaya1122baseball@gmail.com>
2024-06-30 21:56:49 +0900

    マイページ修正

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -6,33 +6,116 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('マイページ'),),
-      body: Column(
-        children: [Row(children: [
-          Image.network(
-            'https://about.fb.com/ja/wp-content/uploads/sites/15/2019/10/new-ig-icon-1.png',
-            width: 100,
-            height: 100,
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Image.network(
+                  'https://about.fb.com/ja/wp-content/uploads/sites/15/2019/10/new-ig-icon-1.png',
+                  width: 100,
+                  height: 100,
 
-          ),
-          Column(
-            children: [
-              Text('7,041'),
-              Text('投稿'),
-            ],
-          ),
-          Column(
-            children: [
-              Text('4.6億'),
-              Text('フォロワー'),
-            ],
-          ),
-          Column(
-            children: [
-              Text('99'),
-              Text('フォロー中'),
-            ],
-          )
-        ],)],
+                ),
+                Spacer(),
+                Column(
+                  children: [
+                    Text(
+                        '7,041',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                        )
+                    ),
+                    Text('投稿'),
+                  ],
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  children: [
+                    Text(
+                        '4.6億',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        )
+                    ),
+                    Text('フォロワー'),
+                  ],
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  children: [
+                    Text(
+                        '99',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        )
+                    ),
+                    Text('フォロー'),
+                  ],
+                )
+              ]
+            ),
+            const SizedBox(height: 16),
+            Text(
+                'instagram',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                )
+            ),
+            Text(
+                '#YoursToMake',
+                style: TextStyle(
+                  color: Colors.blue,
+                )
+            ),
+            Text(
+                'help.instagram',
+                style: TextStyle(
+                  color: Colors.blue,
+                )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text('フォロー中'),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 4),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text('メッセージ'),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 4),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Icon(Icons.keyboard_arrow_down),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
